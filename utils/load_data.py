5c1940cd394ffaac1367c0d6092f8e37ebc2c255
Simon <simon.mack@hec.edu>
2023-03-01 15:19:20 +0100

    Updated .gitignore to include os specific and PyCharm specific files/folders, added Download class with methods to download the road network and the traffic

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,9 +1,52 @@
-import pandas as pd
-import yaml
+from __future__ import annotations
+
 import geopandas as gpd
+import pandas as pd
+
+import io
+import requests
+import yaml
+import zipfile
 
 with open("../config.yaml") as f:
     config = yaml.safe_load(f)
+
+
+class Download:
+    @staticmethod
+    def download_zip_file(url: str, target_folder: str) -> None:
+        """This method downloads a zip file from url and unpacks it to data/target_folder."""
+        zip_file = requests.get(url=url).content
+        zipfile.ZipFile(io.BytesIO(zip_file)).extractall(path=f'data/raw/{target_folder}')
+
+    @staticmethod
+    def download_csv_file(url: str, target_file: str) -> None:
+        """This method downloads a csv file from url and saves it to data/target_file."""
+        csv_file = requests.get(url=url).content
+        with open(f'data/raw/{target_file}', 'wb') as f:
+            f.write(csv_file)
+
+    @classmethod
+    def download_road_network_files(cls, shape_files: bool = True, csv_file: bool = True) -> None:
+        """This method downloads the 'liaisons du réseau routier national' information from data.gouv.fr."""
+        if shape_files:
+            zip_url = 'https://www.data.gouv.fr/fr/datasets/r/92d86944-52e8-44c1-b4cc-b17ac82d70ed'
+            cls.download_zip_file(url=zip_url, target_folder='rrn-2022-shp')
+
+        if csv_file:
+            csv_url = 'https://www.data.gouv.fr/fr/datasets/r/89f8bc85-f923-4540-bbc0-6e010b9b6339'
+            cls.download_csv_file(url=csv_url, target_file='routes-2022.csv')
+
+    @classmethod
+    def download_traffic_files(cls, shape_files: bool = True, csv_file: bool = True) -> None:
+        if shape_files:
+            zip_url = 'https://www.data.gouv.fr/fr/datasets/r/bf95beb9-08e8-4bd1-a734-3d9a66b2caff'
+            cls.download_zip_file(url=zip_url, target_folder='tmja2019-shp')
+
+        if csv_file:
+            csv_url = 'https://www.data.gouv.fr/fr/datasets/r/d5d894b4-b58d-440c-821b-c574e9d6b175'
+            cls.download_csv_file(url=csv_url, target_file='tmja-2019.csv')
+
 
 def load_stations(path:str=None)-> pd.DataFrame:
     if path is None:
