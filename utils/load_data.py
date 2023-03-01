311fe388a5a08fb6763ddac35136d41e80d09f5d
camilleepitalon <camille.epitalon@papernest.com>
2023-03-01 12:07:08 +0100

    load data

--- utils/load_data.py
+++ utils/load_data.py
@@ -7,9 +7,9 @@ with open("../config.yaml") as f:
 
 def load_stations(path:str=None)-> pd.DataFrame:
     if path is None:
-        path = '../' + config['stations_file']
+        path = config['stations_file']
     try:
-        stations= pd.read_csv(path)
+        stations= pd.read_csv('../' + path)
     except FileNotFoundError:
         stations = pd.read_csv(config['stations_file'])
     stations[['lat', 'lon', '1']] = stations.Coordinates.str.split(",", expand = True)
@@ -20,11 +20,63 @@ def load_stations(path:str=None)-> pd.DataFrame:
 
 def load_rrn_vsmap(path:str=None)-> pd.DataFrame:
     if path is None:
-        path = '../' + config['rrn_vsmap_file']
+        path = config['rrn_vsmap_file']
     try:
-        rrn_vsmap = gpd.read_file(path)
+        rrn_vsmap = gpd.read_file('../' + path)
     except FileNotFoundError:
         rrn_vsmap = gpd.read_file(path)
     rrn_vsmap[['dep', 'route', '1', '2', '3', '4']] = rrn_vsmap.route.str.split(" ", expand = True)
     rrn_vsmap = rrn_vsmap.drop(columns = ['1', '2', '3', '4'])
     return rrn_vsmap
+
+
+def load_rrn_bornage(path:str=None)-> pd.DataFrame:
+    if path is None:
+        path = config['rrn_bornage_file']
+    try:
+        rrn_bornage = gpd.read_file('../' + path)
+    except FileNotFoundError:
+        rrn_bornage = gpd.read_file(path)
+    return rrn_bornage
+
+
+def load_regions(path:str=None)-> pd.DataFrame:
+    if path is None:
+        path = config['regions_file']
+    try:
+        regions = gpd.read_file('../' + path)
+    except FileNotFoundError:
+        regions = gpd.read_file(path)
+    regions.drop(regions[regions['nomnewregi']=='Corse'].index, inplace=True)
+    return regions
+
+
+def load_traffic(path:str=None)-> pd.DataFrame:
+    if path is None:
+        path = config['traffic_file']
+    try:
+        traffic = gpd.read_file('../' + path)
+    except FileNotFoundError:
+        traffic = gpd.read_file(path)
+    return traffic
+
+
+def load_depreg(path:str=None)-> pd.DataFrame:
+    if path is None:
+        path = config['depreg_file']
+    try:
+        depreg= pd.read_csv('../' + path)
+    except FileNotFoundError:
+        depreg = pd.read_csv(path)
+    depreg.drop(columns = ['dep_name'], inplace=True)
+    return depreg
+
+
+def load_airesPL(path:str=None)-> pd.DataFrame:
+    if path is None:
+        path = config['airesPL']
+    try:
+        airesPL = gpd.read_file('../' + path)
+    except FileNotFoundError:
+        airesPL = gpd.read_file(path)
+    return airesPL
