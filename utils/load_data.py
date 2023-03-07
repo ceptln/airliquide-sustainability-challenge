dff9090d4e28a272e4641386d006a7918f6cddc9
Simon <simon.mack@hec.edu>
2023-03-07 02:58:59 +0100

    Added methods to Data: _check_exclusivity_of_road_sections to check if all the road sections of a GeoDataFrame are mutually exclusive. Created class Strategies with methods: split_into_areas to assign records to different areas which can be used to distribute stations, _size_buffer_for_distance to find what buffer around a point is needed to envelop a certain amount of traffic * road length. Added methods to Plots: plot_calculated_areas to plot the streets in different colors depending on which area they belong to, _create_legend_handles to create custom legend labels, _get_colors to generate colors.

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import geopandas as gpd
+from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -15,8 +16,11 @@ import os
 import requests
 import time
 from typing import Union
+import warnings
 import yaml
 import zipfile
+
+warnings.simplefilter('ignore', FutureWarning)
 
 
 with open("../config.yaml") as f:
@@ -88,6 +92,18 @@ class Data:
         # Remove the columns we just created
         gdf_stations = gdf_stations.drop(columns=['y', 'x'])
         return gdf_stations
+
+    @staticmethod
+    def _check_exclusivity_of_road_sections(df_traffic: gpd.GeoDataFrame, threshold: Union[int, float] = 1,
+                                            geometry_column: str = 'geometry') -> bool:
+        """This method checks if all the records in df_traffic have mutually exclusive road sections. threshold defines
+        beyond how many meters of overlap a road section is not considered as mutually exclusive."""
+        for index, value in df_traffic[geometry_column]:
+            intersection = df_traffic[geometry_column].intersection(value).length.sum()
+            if (intersection - value.length) > threshold:
+                print(f'{index} has more than {threshold} meter(s) of overlap with other road sections.')
+                return False
+        return True
 
     @staticmethod
     def limit_gdfs_to_main_land_france(df: gpd.GeoDataFrame, df_regions: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
@@ -181,6 +197,14 @@ class Data:
             buffer_traffic = df_traffic.loc[mask, traffic_column].sum()
             df_traffic.loc[index, 'buffer_traffic'] = buffer_traffic
         return df_traffic
+
+    @staticmethod
+    def calculate_total_distance_covered(df_traffic: Union[gpd.GeoDataFrame, pd.DataFrame],
+                                         traffic_column: str = 'truck_tmja',
+                                         road_length_column: str = 'longueur') -> Union[int, float]:
+        """This method calculates the total distance covered by all trucks in df_traffic (in km). It is assumed that
+        the records contain mutually exclusive road sections."""
+        return (df_traffic[traffic_column] * df_traffic[road_length_column] / 1000).sum()
 
     @staticmethod
     def translate_regions_regions_to_official_names(df_regions: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
@@ -338,6 +362,56 @@ class Download:
         cls.download_csv_file(url=csv_url, target_file='depreg.csv')
 
 
+class Strategies:
+    @classmethod
+    def split_into_areas(cls, df_traffic: gpd.GeoDataFrame, n_areas: int, traffic_column: str = 'tmja',
+                         road_length_column: str = 'longueur',
+                         rank_column: str = 'buffer_traffic',
+                         buffer_step: Union[int, float] = 10000) -> gpd.GeoDataFrame:
+        """This method creates n_areas areas that each contain at least 1 / n_areas * total distance covered by trucks
+        in all of France. The output DataFrame will have n_areas new columns, each telling which records belong to
+        an area. One record can belong to multiple areas.
+        The downside of this method is that some records (=road sections) do not belong to any area, especially the
+        sections close to the border are affected by this.
+        It is assumed that the geometry in df_traffic is stored in a column called 'geometry'."""
+        df_traffic = df_traffic.copy(deep=True)
+        total_distance = Data.calculate_total_distance_covered(df_traffic)
+        distance_per_area = total_distance / n_areas
+        created_areas = 0
+        reference = df_traffic[rank_column].argmax()
+        mask = pd.Series(index=df_traffic.index)
+        mask.loc[:] = False
+        mask = mask.astype(bool)
+        areas = dict()
+        while created_areas < n_areas:
+            buffer_size, used_records_mask = cls._size_buffer_for_distance(df_traffic=df_traffic, index=reference,
+                                                                           target_distance=distance_per_area,
+                                                                           buffer_step=buffer_step)
+            areas[reference] = buffer_size
+            mask += used_records_mask
+            reference = df_traffic.loc[~mask, rank_column].sort_values(ascending=False).index[0]
+            created_areas += 1
+            df_traffic.loc[used_records_mask, f'area_{created_areas}'] = True
+            df_traffic.loc[~used_records_mask, f'area_{created_areas}'] = False
+        df_traffic['no_area'] = ~mask
+        return df_traffic
+
+    @staticmethod
+    def _size_buffer_for_distance(df_traffic: gpd.GeoDataFrame, index: int, target_distance: Union[int, float],
+                                  buffer_step: Union[int, float] = 10000) -> tuple[Union[int, float], pd.Series]:
+        """This method provides the size of the buffer that is needed around the record at index to cover
+        target_distance when summing 'longueur' * 'tmja' over all road sections inside the buffer."""
+        covered_distance = 0
+        buffer = buffer_step
+        area = df_traffic.loc[index, 'geometry'].buffer(buffer)
+        while covered_distance < target_distance:
+            mask = df_traffic.intersects(area)
+            covered_distance = Data.calculate_total_distance_covered(df_traffic.loc[mask, :])
+            buffer += buffer_step
+            area = df_traffic.loc[index, 'geometry'].buffer(buffer)
+        return buffer - buffer_step, mask
+
+
 class Plots:
     @staticmethod
     def plot_roads_over_regions(df_traffic: gpd.GeoDataFrame, df_regions: gpd.GeoDataFrame) -> None:
@@ -360,6 +434,66 @@ class Plots:
         plot_data.plot(ax=ax, column='tmja', edgecolor='black', legend=True, legend_kwds={'label': 'Traffic per day'})
         ax.set_title('Daily traffic in France')
         plt.show()
+
+    @classmethod
+    def plot_calculated_areas(cls, df_areas: gpd.GeoDataFrame, df_regions: gpd.GeoDataFrame) -> None:
+        """This method plots the roads on top of France, with a color indicating which area a road section belongs to.
+        df_areas is assumed to contain a geometry column as well as one or more boolean columns starting with 'area_'
+        telling which record belong to which area (as output by Strategies.split_into_areas()), as well as a column
+        'buffer_traffic' (as output by Data.add_buffer_traffic)."""
+        df_areas = df_areas.copy(deep=True)
+        # Creating geometries that represent a buffer of 10 km around the road sections
+        df_areas['buffer_geometry'] = df_areas.buffer(10000)
+        df_areas = df_areas.set_geometry('buffer_geometry')
+
+        fig, ax = plt.subplots()
+        # Drawing the outline of the French regions
+        df_regions.plot(ax=ax)
+        # The following line would have to be uncommented if one wanted to see a heat map of the traffic
+        # df_areas.plot(ax=ax, column='buffer_traffic', legend=True, cmap='hot')
+
+        # Creating list of area columns
+        areas = [col for col in df_areas if col.startswith('area_')] + ['no_area']
+        # Creating a list of len(areas) columns
+        colors = cls._get_colors(len(areas))
+        # Drawing the roads with a buffer of 10 km, each with a different color
+        for area, color in zip(areas, colors):
+            df_areas[df_areas[area]].plot(ax=ax, color=color, alpha=0.5)
+        # Adding the legend for the different areas
+        custom_handles = cls._create_legend_handles(colors=colors, labels=areas)
+        existing_handles, _ = ax.get_legend_handles_labels()
+        ax.legend(
+            handles=[
+                *existing_handles,
+                *custom_handles],
+            title="",
+            loc=(1.04, 0.9),
+            ncol=1,
+            frameon=False,
+            shadow=False)
+        fig.tight_layout()
+
+    @staticmethod
+    def _create_legend_handles(colors: list[Union[str, tuple[float, float, float, float]]],
+                               labels: list[str]) -> list[Line2D]:
+        assert len(colors) == len(labels), 'colors and labels have to have the same length.'
+        legend_handles = list()
+        for color, label in zip(colors, labels):
+            legend_handles.append(Line2D(
+                xdata=[],
+                ydata=[],
+                color=color,
+                linewidth=4,
+                label=label))
+        return legend_handles
+
+    @staticmethod
+    def _get_colors(n_colors: int) -> list[tuple[float, float, float, float]]:
+        cmap = plt.cm.get_cmap('plasma', n_colors + 1)
+        colors = list()
+        for i in range(n_colors):
+            colors.append(cmap(i))
+        return colors
 
 
 def load_stations(path: str = None) -> pd.DataFrame:
